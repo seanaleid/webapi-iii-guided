0d26bf8d2ea35648f262ef03f1e7e8b75c28f734
seanaleid <seanaleid@gmail.com>
2019-10-30 13:05:32 -0400

    Adds middleware from lecture

---
+++ notes.md
@@ -0,0 +1,24 @@
+# Middleware notes
+
+## Jargon
+
+Separation of Concerns:
+
+_we do not write code for the computer, code is a communication device, a way to reveal our intentions to the next developer_
+
+_optimization of readability_
+
+***EVERYTHING IS MIDDLEWARE!!***
+
+## TYPES (BASED ON HOW WE GOT IT OR WHO BUILT IT)
+
+- built-in: included with express. ex: `express.json()`
+- third party: must be installed from `npm`
+- custom
+
+## Types (based on how it's being used)
+
+- global: runs on every request that comes in to the server
+- 
+
+***Order matters, it goes top to bottom and left to right.***
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+// los tres amigos
+function dateLogger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.originalUrl} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
+}
+
+// global middleware
+server.use(helmet()); // third-party
+server.use(express.json()); // built-in
+server.use(dateLogger());
 
 server.use('/api/hubs', hubsRouter);
 
